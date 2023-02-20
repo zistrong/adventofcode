98a1dc2ad6afa79297be29c00a8b85f030d6c326
zistrong <zistrong@163.com>
2023-02-20 09:24:37 +0800

    day11 refactor

--- src/test/java/com/zistrong/adventofcode/Day11.java
+++ src/test/java/com/zistrong/adventofcode/Day11.java
@@ -1,15 +1,15 @@
 package com.zistrong.adventofcode;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 
 public class Day11 {
 
@@ -27,16 +27,14 @@ public class Day11 {
             }
         }
 
-        Collections.sort(this.monkeys, (t1, t2) -> {
-            return t1.getInspectTimes() <= t2.getInspectTimes() ? 1 : -1;
-        });
+        this.monkeys.sort(Comparator.comparingInt(Monkey::getInspectTimes).reversed());
 
         Assert.assertEquals(monkeys.get(0).getInspectTimes() * monkeys.get(1).getInspectTimes(), 111210);
     }
 
     @Before
     public void init() throws IOException {
-        this.steps = Files.readAllLines(Path.of("./src/test/resources/", "day11.input"));
+        List<String> steps = Files.readAllLines(Path.of("./src/test/resources/", "day11.input"));
 
         monkeys = new ArrayList<>();
         for (int i = 0; i < steps.size(); i = i + 7) {
@@ -46,17 +44,15 @@ public class Day11 {
                 monkey.getWorriies().offer(Integer.parseInt(s.trim()));
             }
             String opLine = steps.get(i + 2);
-            monkey.setId(monkeys.size());
             monkey.setOperation(opLine.split(":")[1].replace("new = ", "").trim());
-            monkey.setDivivedNumber(Integer.parseInt(steps.get(i + 3).replace("Test: divisible by ", "").trim()));
+            monkey.setDividedNumber(Integer.parseInt(steps.get(i + 3).replace("Test: divisible by ", "").trim()));
             monkey.setPositiveTarget(
                     Integer.parseInt(steps.get(i + 4).replace("If true: throw to monkey ", "").trim()));
-            monkey.setNegtiveTarget(
+            monkey.setNegativeTarget(
                     Integer.parseInt(steps.get(i + 5).replace("If false: throw to monkey ", "").trim()));
             monkeys.add(monkey);
         }
     }
 
-    private List<String> steps;
     List<Monkey> monkeys;
 }
--- src/test/java/com/zistrong/adventofcode/Monkey.java
+++ src/test/java/com/zistrong/adventofcode/Monkey.java
@@ -6,15 +6,13 @@ import java.util.Queue;
 
 public class Monkey implements Serializable {
 
-    private int id;
+    private final Queue<Integer> worriies = new ArrayDeque<>();
 
-    private Queue<Integer> worriies = new ArrayDeque<>();
-
-    private int divivedNumber;
+    private int dividedNumber;
 
     private int positiveTarget;
 
-    private int negtiveTarget;
+    private int negativeTarget;
 
     private int target;
 
@@ -37,29 +35,17 @@ public class Monkey implements Serializable {
         String op = operation.split(" ")[1].trim();
         int opNumber2 = Integer.parseInt(operation.split(" ")[2].trim().replace("old", String.valueOf(num)));
         switch (op) {
-            case "+": {
-                num = opNumber1 + opNumber2;
-                break;
-            }
-            case "-": {
-                num = opNumber1 - opNumber2;
-                break;
-            }
-            case "*": {
-                num = opNumber1 * opNumber2;
-                break;
-            }
-            case "/": {
-                num = opNumber1 / opNumber2;
-                break;
-            }
+            case "+" -> num = opNumber1 + opNumber2;
+            case "-" -> num = opNumber1 - opNumber2;
+            case "*" -> num = opNumber1 * opNumber2;
+            case "/" -> num = opNumber1 / opNumber2;
         }
         // you calculate
         num = num / 3;
-        if (num % divivedNumber == 0) {// check target
+        if (num % dividedNumber == 0) {// check target
             target = positiveTarget;
         } else {
-            target = negtiveTarget;
+            target = negativeTarget;
         }
         this.inspectTimes++;// record time
         this.popWorry = num;
@@ -79,52 +65,24 @@ public class Monkey implements Serializable {
         return this.worriies.toString();
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public Queue<Integer> getWorriies() {
         return worriies;
     }
 
-    public void setWorriies(Queue<Integer> worriies) {
-        this.worriies = worriies;
-    }
-
-    public int getDivivedNumber() {
-        return divivedNumber;
-    }
-
-    public void setDivivedNumber(int divivedNumber) {
-        this.divivedNumber = divivedNumber;
-    }
-
-    public int getPositiveTarget() {
-        return positiveTarget;
+    public void setDividedNumber(int dividedNumber) {
+        this.dividedNumber = dividedNumber;
     }
 
     public void setPositiveTarget(int positiveTarget) {
         this.positiveTarget = positiveTarget;
     }
 
-    public int getNegtiveTarget() {
-        return negtiveTarget;
-    }
-
-    public void setNegtiveTarget(int negtiveTarget) {
-        this.negtiveTarget = negtiveTarget;
+    public void setNegativeTarget(int negativeTarget) {
+        this.negativeTarget = negativeTarget;
     }
 
     public int getTarget() {
         return target;
-    }
-
-    public String getOperation() {
-        return operation;
     }
 
     public void setOperation(String operation) {
