af2009046529ca7524f50488688ac9146ed81897
zistrong <zistrong@163.com>
2023-03-03 17:27:25 +0800

    day11 todo

--- src/test/java/com/zistrong/adventofcode/Monkey.java
+++ src/test/java/com/zistrong/adventofcode/Monkey.java
@@ -6,8 +6,28 @@ import java.util.Queue;
 
 public class Monkey implements Serializable {
 
-    private final Queue<Integer> worriies = new ArrayDeque<>();
 
+    static class Worry {
+        int value;
+        boolean inspected = false;
+
+        public Worry(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Worry{" +
+                    "value=" + value +
+                    ", inspected=" + inspected +
+                    '}';
+        }
+    }
+
+    private final Queue<Integer> worries = new ArrayDeque<>();
+    private final Queue<Worry> worriesQueue = new ArrayDeque<>();
+
+    int id;
     private int dividedNumber;
 
     private int positiveTarget;
@@ -19,16 +39,72 @@ public class Monkey implements Serializable {
     private String operation;
 
     private int popWorry;
+    private Worry popWorryObj;
 
     private int inspectTimes;
 
-    public boolean inspect() {
+    private boolean inspected = false;
 
-        if (this.worriies.isEmpty()) {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean inspect2(boolean allInspected) {
+
+        if (this.worriesQueue.isEmpty()) {
             return false;
         }
 
-        int num = this.worriies.poll();//pop worry
+        Worry worry = this.worriesQueue.poll();//pop worry
+        int num = worry.value;
+
+        //1 monkey inspect item
+        //calculate value, you worry is calculated
+        int opNumber1 = Integer.parseInt(operation.split(" ")[0].trim().replace("old", String.valueOf(num)));
+        String op = operation.split(" ")[1].trim();
+        int opNumber2 = Integer.parseInt(operation.split(" ")[2].trim().replace("old", String.valueOf(num)));
+        switch (op) {
+            case "+" -> num = opNumber1 + opNumber2;
+            case "-" -> num = opNumber1 - opNumber2;
+            case "*" -> num = opNumber1 * opNumber2;
+            case "/" -> num = opNumber1 / opNumber2;
+        }
+        // after than you worry is divived
+        // you calculate
+//        if (!(this.worriesQueue.stream().allMatch(item -> item.inspected))) {
+//            num = num / 3;
+//        }
+
+        if (worry.inspected) {
+            num = num / 3;
+        }
+        if (num % dividedNumber == 0) {// check target
+            target = positiveTarget;
+        } else {
+            target = negativeTarget;
+        }
+
+        this.inspected = true;
+        worry.inspected = true;
+        this.inspectTimes++;// record time
+        worry.value = num;
+
+        this.popWorryObj = worry;
+        return true;
+
+    }
+
+    public boolean inspect() {
+
+        if (this.worries.isEmpty()) {
+            return false;
+        }
+
+        int num = this.worries.poll();//pop worry
 
         //calculate value
         int opNumber1 = Integer.parseInt(operation.split(" ")[0].trim().replace("old", String.valueOf(num)));
@@ -55,18 +131,29 @@ public class Monkey implements Serializable {
 
     public void push(int worry) {
 
-        this.worriies.offer(worry);
+        this.worries.offer(worry);
+        this.worriesQueue.offer(new Worry(worry));
+
+    }
+
+    public void push(Worry worry) {
+
+        this.worriesQueue.offer(worry);
 
     }
 
     @Override
     public String toString() {
 
-        return this.worriies.toString();
+        return this.id + ":" + this.worriesQueue;
     }
 
-    public Queue<Integer> getWorriies() {
-        return worriies;
+    public Queue<Integer> getWorries() {
+        return worries;
+    }
+
+    public Queue<Worry> getWorriesObjs() {
+        return worriesQueue;
     }
 
     public void setDividedNumber(int dividedNumber) {
@@ -91,6 +178,10 @@ public class Monkey implements Serializable {
 
     public int getPopWorry() {
         return popWorry;
+    }
+
+    public Worry getPopWorryObj() {
+        return popWorryObj;
     }
 
     public int getInspectTimes() {
