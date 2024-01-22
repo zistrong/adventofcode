c39c7573a76009b5f8d4f19dac08ecd765d24d9c
zistrong <zistrong@163.com>
2024-01-22 14:13:06 +0800

    2023 day19 part1 refactor

--- src/test/java/com/zistrong/adventofcode2023/Day19.java
+++ src/test/java/com/zistrong/adventofcode2023/Day19.java
@@ -1,5 +1,8 @@
 package com.zistrong.adventofcode2023;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,121 +11,69 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 public class Day19 {
 
 
     @Test
     public void part1() throws IOException {
         List<String> list = Files.readAllLines(Path.of("./src/test/resources/2023/", "day19.input"));
-        Map<String, WorkFlow> workFlows = new HashMap<>();
-
-        List<Rate> rates = new ArrayList<>();
-
+        Map<String, String[]> workFlows = new HashMap<>();
+        List<Map<String, Integer>> rates = new ArrayList<>();
         boolean israte = false;
 
         for (String string : list) {
-            
-            if(string.isBlank()) {
+
+            if (string.isBlank()) {
                 israte = true;
                 continue;
             }
-            if(israte) {
-                String[] args  = string.replace("{", "").replace("}", "").split(",");
-                Rate rate = new Rate();
-                for(String arg : args) {
-                    if(arg.startsWith("x")) {
-                        rate.x = Integer.parseInt(arg.replace("x=", ""));
-                    } else if(arg.startsWith("m")) {
-                        rate.m = Integer.parseInt(arg.replace("m=", ""));
-                    }else if(arg.startsWith("a")) {
-                        rate.a = Integer.parseInt(arg.replace("a=", ""));
-                    }else if(arg.startsWith("s")) {
-                        rate.s = Integer.parseInt(arg.replace("s=", ""));
-                    }
+            if (israte) {
+                String[] args = string.replace("{", "").replace("}", "").split(",");
+                Map<String, Integer> rate = new HashMap<>();
+                for (String arg : args) {
+
+                    rate.put(String.valueOf(arg.charAt(0)), Integer.parseInt(arg.substring(2)));
                 }
                 rates.add(rate);
             } else {
                 String name = string.split("\\{")[0];
-                WorkFlow workFlow = new WorkFlow();
                 String[] commands = string.split("\\{")[1].replace("}", "").split(",");
-                for(String command : commands) {
-                    workFlow.conditions.add(command);
-                }
-                workFlows.put(name, workFlow);
+                workFlows.put(name, commands);
             }
         }
-        long count = 0L;
-        
-        WorkFlow currentFlow = workFlows.get("in");
-        for(Rate item : rates) {
+        long sum = 0L;
+        for (Map<String, Integer> item : rates) {
             String result = "";
+            String[] currentFlow = workFlows.get("in");
             while (!result.equals("A") && !result.equals("R")) {
                 result = getTarget(currentFlow, item);
                 currentFlow = workFlows.get(result);
             }
-            if(result.equals("A")) {
-                count += item.getSum();
+            if (result.equals("A")) {
+                sum += item.values().stream().mapToInt(i -> i).sum();
             }
-            currentFlow = workFlows.get("in");
         }
 
-        Assert.assertEquals(420739L, count);
+        Assert.assertEquals(420739L, sum);
 
     }
 
-    private String getTarget(WorkFlow currentFlow, Rate rate) {
-
-        for(String command : currentFlow.conditions) {
-            if(!command.contains(":")) {
+    private String getTarget(String[] conditions, Map<String, Integer> rate) {
+        for (String command : conditions) {
+            if (!command.contains(":")) {
                 return command;
             } else {
                 String co = command.split(":")[0];
                 String re = command.split(":")[1];
-                if(co.startsWith("x>") && rate.x> Integer.parseInt(co.replace("x>", "")) ) {
+                String key = String.valueOf(co.charAt(0));
+                char c = co.charAt(1);
+                if (c == '>' && rate.get(key) > Integer.parseInt(co.substring(2))
+                        || (c == '<' && rate.get(key) < Integer.parseInt(co.substring(2)))) {
                     return re;
-                } else if (co.startsWith("x<")&& rate.x< Integer.parseInt(co.replace("x<", ""))) {
-                    return re;
-
-                }else if (co.startsWith("m>")&& rate.m> Integer.parseInt(co.replace("m>", ""))) {
-                    return re;
-
-                }else if (co.startsWith("m<")&& rate.m< Integer.parseInt(co.replace("m<", ""))) {
-                    return re;
-
-                }else if (co.startsWith("a>")&& rate.a> Integer.parseInt(co.replace("a>", ""))) {
-                    return re;
-
-                }else if (co.startsWith("a<")&& rate.a< Integer.parseInt(co.replace("a<", ""))) {
-                    return re;
-
-                }else if (co.startsWith("s>")&& rate.s> Integer.parseInt(co.replace("s>", ""))) {
-                    return re;
-
-                }else if (co.startsWith("s<")&& rate.s< Integer.parseInt(co.replace("s<", ""))) {
-                    return re;
-
                 }
             }
         }
         return "R";
     }
 
-    private static class Rate {
-        int x;
-        int m;
-        int a;
-        int s;
-
-        public long getSum() {
-            return x + m + a +s+0L;
-        }
-    }
-
-    private static class WorkFlow {
-        List<String> conditions = new ArrayList<>();
-    }
-    
 }
